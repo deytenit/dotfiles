d063cd5773b16869908bbc976f81c1fe5346ed88
Vladimir Eremin <personal@ermnvldmr.com>
2024-05-03 14:33:54 +0300

    Semyon the Goose

---
+++ .config/nvim/lua/plugins/misc.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "andweeb/presence.nvim",
+    opts = {
+      buttons = { { label = "whoami", url = "https://github.com/deytenit" } },
+      workspace_text = "Connected to Daedalus:Global",
+      editing_text = "MAJESTIC//GLOBAL//12.12.12",
+      reading_text = "SECURE//IIS.INFTRANS.SYS//UEU",
+    },
+  },
+}
