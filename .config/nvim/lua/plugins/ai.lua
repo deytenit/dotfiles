0bf91d9667e42e33a114b504a9c5a50ba7141e51
Vladimir Eremin <personal@ermnvldmr.com>
2025-05-06 11:01:59 +0300

    fix: Support large projects in LSP & make compatible semantic script

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -27,10 +27,74 @@ return {
       local use_yalm = vim.v.shell_error == 0
 
       local adapters = {
-        yalm_deepseek = function()
+        yalm_deepseek_v3 = function()
           return require("codecompanion.adapters").extend("openai_compatible", {
             env = {
               url = "http://zeliboba.yandex-team.ru/balance/communal-deepseek-v3-0324-in-yt",
+              api_key = "cmd:cat ~/.yalm_token",
+              chat_url = "/v1/chat/completions",
+            },
+            schema = {
+              model = {
+                default = "deepseek",
+              },
+              temperature = {
+                order = 2,
+                mapping = "parameters",
+                type = "number",
+                optional = true,
+                default = 0.8,
+                desc = "What sampling temperature to use, between 0 and 2. Higher values like 0.8 will make the output more random, while lower values like 0.2 will make it more focused and deterministic. We generally recommend altering this or top_p but not both.",
+                validate = function(n)
+                  return n >= 0 and n <= 2, "Must be between 0 and 2"
+                end,
+              },
+              max_completion_tokens = {
+                order = 3,
+                mapping = "parameters",
+                type = "integer",
+                optional = true,
+                default = nil,
+                desc = "An upper bound for the number of tokens that can be generated for a completion.",
+                validate = function(n)
+                  return n > 0, "Must be greater than 0"
+                end,
+              },
+              stop = {
+                order = 4,
+                mapping = "parameters",
+                type = "string",
+                optional = true,
+                default = nil,
+                desc = "Sets the stop sequences to use. When this pattern is encountered the LLM will stop generating text and return. Multiple stop patterns may be set by specifying multiple separate stop parameters in a modelfile.",
+                validate = function(s)
+                  return s:len() > 0, "Cannot be an empty string"
+                end,
+              },
+              logit_bias = {
+                order = 5,
+                mapping = "parameters",
+                type = "map",
+                optional = true,
+                default = nil,
+                desc = "Modify the likelihood of specified tokens appearing in the completion. Maps tokens (specified by their token ID) to an associated bias value from -100 to 100. Use https://platform.openai.com/tokenizer to find token IDs.",
+                subtype_key = {
+                  type = "integer",
+                },
+                subtype = {
+                  type = "integer",
+                  validate = function(n)
+                    return n >= -100 and n <= 100, "Must be between -100 and 100"
+                  end,
+                },
+              },
+            },
+          })
+        end,
+        yalm_deepseek_r1 = function()
+          return require("codecompanion.adapters").extend("openai_compatible", {
+            env = {
+              url = "http://zeliboba.yandex-team.ru/balance/deepseek_r1",
               api_key = "cmd:cat ~/.yalm_token",
               chat_url = "/v1/chat/completions",
             },
@@ -115,8 +179,8 @@ return {
       local inline_adapter
       if use_yalm then
         vim.notify("CodeCompanion: Using 'yalm' based adapter (arc root)", vim.log.levels.INFO)
-        main_adapter = "yalm_deepseek"
-        inline_adapter = "yalm_deepseek"
+        main_adapter = "yalm_deepseek_r1"
+        inline_adapter = "yalm_deepseek_v3"
       else
         vim.notify("CodeCompanion: Using 'copilot' adapter", vim.log.levels.INFO)
         main_adapter = "copilot_gemini_pro"
