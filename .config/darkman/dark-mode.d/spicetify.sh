8186378d7c405d438e55bfe00f5ffd30d2b23282
Vladimir Eremin <personal@ermnvldmr.com>
2025-06-27 18:57:24 +0300

    fix: Remove spicetify since moving to jellyfin

--- .config/darkman/dark-mode.d/spicetify.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Update spicetify and spotify using yay
-yay -Syu --noconfirm spicetify-cli spotify
-
-# Backup and apply spicetify configuration
-spicetify backup apply
-spicetify config current_theme catppuccin color_scheme mocha
-spicetify -n apply
