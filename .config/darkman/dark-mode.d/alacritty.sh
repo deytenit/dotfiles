28c65125e439bac23ac713deb61c3e23b06c2986
Vladimir Eremin <personal@ermnvldmr.com>
2026-01-04 15:57:07 +0300

    auto-sync: 2026-01-04 15:57:07

--- .config/darkman/dark-mode.d/alacritty.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-sed -i '/# BEGIN_ALACRITTY_THEME/,/# END_ALACRITTY_THEME/c\
-# BEGIN_ALACRITTY_THEME\
-general.import = [ "~/.config/alacritty/dark.toml" ]\
-# END_ALACRITTY_THEME' ~/.config/alacritty/.theme.toml
--- .config/darkman/light-mode.d/alacritty.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-sed -i '/# BEGIN_ALACRITTY_THEME/,/# END_ALACRITTY_THEME/c\
-# BEGIN_ALACRITTY_THEME\
-general.import = [ "~/.config/alacritty/light.toml" ]\
-# END_ALACRITTY_THEME' ~/.config/alacritty/.theme.toml
