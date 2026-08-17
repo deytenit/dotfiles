d393d1cfa0b02b745540f28e9970d37572e62543
Vladimir Eremin <personal@ermnvldmr.com>
2026-08-17 14:32:50 +0300

    auto-sync: 2026-08-17 14:32:50

--- .config/nvim/lua/plugins/project.lua
+++ .config/nvim/lua/plugins/project.lua
@@ -2,6 +2,23 @@ return {
   {
     "https://github.com/DrKJeff16/project.nvim",
     main = "project",
-    opts = {},
+    lazy = false,
+    dependencies = {
+      "ibhagwan/fzf-lua",
+      "folke/snacks.nvim",
+    },
+    opts = {
+      fzf_lua = { enabled = true },
+      snacks = { enabled = true },
+    },
+    keys = {
+      { "<leader>fp", "<cmd>Project fzf-lua<cr>", desc = "Projects" },
+    },
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = function(_, opts)
+      table.insert(opts.sections.lualine_b, { "project" })
+    end,
   },
 }
